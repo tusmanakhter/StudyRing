5f2383979945c7364ee2e3f6e1338b828dc98b63
Zackkogan <zackkogan@hotmail.com>
2017-01-30 18:30:20 -0500

    Update Login.php

--- Login.php
+++ Login.php
@@ -23,6 +23,42 @@
 <h1>Page heading</h1>
 <div id="Content"> 
 	<div id="ContentLeft"></div>
+		<form method="post">
+			<fieldset>
+				<legend>Please enter your username and password:</legend> <!--form for users to enter username and password for validation-->
+				<label>Username:</label>
+				<input type ="text" name="Username" id="Username"/><br />
+				<label>Password:</label>
+				<input type ="text" name="password" id="password"/>
+			</fieldset>
+				<input type = "submit" value = "Submit" />
+				<input type = "reset" value = "Reset" />
+		</form>
+
+		<?php
+		$accounts = file("users.txt"); //linking to users.txt
+		$control = 0; //prevents page from going back to login page if proper username + password entered. Also prevents redirect loop.
+
+			for ($line = 0; $line < sizeof($accounts); $line++){
+				$userpass = preg_split("/:/", $accounts[$line]);
+
+				if (($_POST["Username"] == $userpass[0]) && ($_POST["password"] == trim($userpass[1]))){ //checking that user and password data is correct
+
+					echo '<script language="javascript">';
+					echo 'alert("Sucessfully logged in")'; //alerts user that login was successful
+					echo '</script>';
+					$control = 1; //set control variable to 1 to indicate successful login
+					header ("Location: template.php"); //sends user to template.php page after successful login. 
+				}
+			}
+
+	if ($control = 0){ //unsuccessful login
+		echo '<script language="javascript">';
+		echo "alert('Error. Username or Password incorrect. Plesae try again.')"; //error message to the user
+		echo '</script>';
+		header ("Location: Login.php"); //redirect back to login page
+	}
+?>
     <div id = "ContentRight"> </div>
 </div>
 <div id="Footer"> </div>
