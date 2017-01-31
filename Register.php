9ed81b24d409bfa4bb01f8cfd7ce1ed2b522595f
Zackkogan <zackkogan@hotmail.com>
2017-01-30 19:03:55 -0500

    Update Register.php
    -removed div tag on line 60 (all prior div tags were closed)
    -added PHP database I/O to add created accounts in the database. Passwords must contain at least 8 characters and must contain at least one letter and number for security purposes.
    -If "Unidentified Error" comes up in testing, then I messed up somewhere. Probably with making sure password has a number and the last else{} block would be my guess for possible error.

--- Register.php
+++ Register.php
@@ -42,21 +42,79 @@
           <input name="email" type="email" required="required" id="email"></td>
         </tr>
         <tr>
-          <td><label for="textfield3">Username:</label>
-          <input name="textfield3" type="text" required="required" id="textfield3"></td>
+          <td><label for="textfield3">Username (may only contain letters or numbers):</label>
+          <input name="username" type="text" required="required" id="username"></td>
         </tr>
         <tr>
-          <td><label for="password">Password:</label>
+          <td><label for="password">Password (must be at least 8 characters long, contain at least one letter and one number):</label>
           <input name="password" type="password" required="required" id="password"></td>
         </tr>
         <tr>
-          <td><input type="submit" name="submit" id="submit" value="Submit"></td>
+          <td><input type="submit" value="Submit"></td><td><input type="reset" value="Reset">
         </tr>
       </tbody>
     </table>
   </form>
 </div>
 <div id="Footer"> </div>
-</div>
+	
+<?php 
+	$file = fopen("users.txt", "a+"); //opens users.txt file
+
+	$user = ($_POST["Username"]); 
+	$pass = ($_POST["password"]);
+	$accounts = file("users.txt");
+
+	if ((strlen($user) != 0) && (strlen($pass) != 0)){
+
+	for ($line = 0; $line < sizeof($accounts); $line++){
+		$userpass = preg_split("/:/", $accounts[$line]);
+
+		if (strcmp($user, $userpass[0]) == 0){ //makes sure username isnt already in database
+			echo '<script language="javascript">';
+			echo 'alert("Error. Username already exists in database. Please try again.")';
+			echo '</script>';
+		}
+	}
+	else if (preg_match("/[^A-Za-z0-9]/", $user)){ //makes sure only letters and numbers in username
+			echo '<script language="javascript">';
+			echo "alert('Error. Username may only contain letters and numbers. Please try again.')";
+			echo '</script>';
+		}
+	else if (preg_match("/[^A-Za-z0-9]/", $pass)){ //makes sure only letters and numbers in password
+			echo '<script language="javascript">';
+			echo 'alert("Error. Password may only contain letters and numbers. Please try again.")';
+			echo '</script>';
+		}
+	else if (strlen($pass) < 8){ //makes sure password is at least 8 characters long
+			echo '<script language="javascript">';
+			echo 'alert("Error. Password must contain at least four characters. Please try again.")';
+			echo '</script>';
+		}		
+	else if ((preg_match("/[^0-9]/", $pass)) == false){//gives an error message if password contains no number
+			echo '<script language="javascript">';
+			echo 'alert("Error. Password must contain at least one letter and one number. Please try again.")';
+			echo '</script>';
+	}
+	else if ((preg_match("/[^A-Za-z]/", $pass)) && (preg_match("/[^0-9]/", $pass))){ 
+		$file = fopen("users.txt", "a");
+		fwrite($file, $user); //adding user and password into databse
+		fwrite($file, ":");
+		fwrite($file, $pass);
+		fwrite($file, "\n");
+			echo '<script language="javascript">';
+			echo 'alert("Account successfully created. You may now log in with your new account from the Login page.")'; //user notification that account has been made
+			echo '</script>';
+		header ("Location: Login.php"); //sends user to the login page to log in with their new account
+		}
+	else {
+		echo '<script language="javascript">';
+		echo 'alert("Unidentified Error. Something went wrong somewhere, contact site admin for maintenance.")'; //this error message should never come up. If it does, something went wrong somewhere and needs fixing.
+		echo '</script>';
+		}
+	}
+	fclose($file); //closes users.txt file
+?>
+	
 </body>
 </html>
