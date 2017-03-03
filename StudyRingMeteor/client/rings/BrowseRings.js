ac9bb5fe42c3607360c2962096855a1b4f4ddf1b
tusmanakhter <tusman96@hotmail.com>
2017-03-03 12:06:35 -0500

    Added search functionality
    A search bar at the top of the "Browse Rings" pages allow users to
    search by name and tag. #38

--- StudyRingMeteor/client/rings/BrowseRings.js
+++ StudyRingMeteor/client/rings/BrowseRings.js
@@ -7,6 +7,17 @@ Template.BrowseRings.onCreated(function(){
 
 Template.BrowseRings.helpers({
     rings: ()=> {
-        return Rings.find({});
+        var regexp = new RegExp(Session.get('search/keyword'), 'i');
+        var selector = {$or: [
+            {name: regexp},
+            {tags: {$elemMatch: {name: regexp}}}
+        ]};
+        return Rings.find(selector);
     }
+});
+
+Template.BrowseRings.events({
+  'keyup #search': function(event) {
+    Session.set('search/keyword', event.target.value);
+  }
 });
