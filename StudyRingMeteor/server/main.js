c102544aba00213a5e4418d93ef2b14b30810ac3
tusmanakhter <tusman96@hotmail.com>
2017-03-09 01:20:43 -0500

    Added reset password functionality
    - Added reset password HTML page
    - Users can now recieve a link to their email to reset password
    - SMTP email server configured to send out emails
    - Fixed login page (it was broken)
    
    This is linked with issue #26

--- StudyRingMeteor/server/main.js
+++ StudyRingMeteor/server/main.js
@@ -2,4 +2,7 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
-});
+  if(Meteor.isServer){
+    process.env.MAIL_URL="smtp://StudyRingConcordia%40gmail.com:soen341concordia@smtp.gmail.com:465/"; 
+  }
+})
