feca5c5d5e0f2a67a2ccd2e784467c0f14c50190
tusmanakhter <tusman96@hotmail.com>
2017-03-15 18:33:09 -0400

    Bug fix, removing unecessary files
    - Bootstrap and font awesome are already included in our meteor
    packages, added them seperately will cause issues
    - Fixed many bugs in various layouts

--- StudyRingMeteor/test/rings.tests.js
+++ StudyRingMeteor/test/rings.tests.js
@@ -14,4 +14,28 @@ if (Meteor.isServer) {
           assert.equal(arr.length, 0);
       });
   })
+
+//Trying to write a test here
+//   describe( 'Add a Ring', function() {
+//     afterEach( function() {
+//         var ring = Rings.findOne( { Name: 'Name', desc: "Desc"} );
+//         if ( ring ) {
+//             Rings.remove( ring._id );
+//         }
+//     });
+
+//     it( 'should create a new ring @watch', function() {
+//         browser.url( 'http://localhost:3000/rings' )
+//             .setValue( '[name=tags', "Tag")
+//             .setValue( '[name="title"]', 'Title' )
+//             .setValue( '[name="dec"]', 'Peter Bevelin' )
+//             .submitForm( 'form' );
+
+//         var getRing = server.execute( function() {
+//         return Rings.findOne( { title: 'Title' } );
+//         });
+
+//         expect( getBook.title ).to.equal( 'Seeking Wisdom' );
+//         });
+//     })
 }
